7579bf5758a9830bfa3a74761dbe09dc2629f528
lukasjaronis <jaronisluke13@gmail.com>
2019-12-09 17:07:22 -0500

    createContext ProductContext created

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-const CartContext = createContext();
+const ProductContext = createContext();
 
-export default CartContext;
+export default ProductContext;
